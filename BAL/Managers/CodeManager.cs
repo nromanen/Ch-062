b5349fcfe0a91bf77a746654a8e4ca8086c47136
rychkovcv <rychkovcv@gmail.com>
2018-04-26 18:09:35 +0300

    #30 User able to start doing exercise from exercise page, code executed results now saving in DB, user able to work with latest version of exercise code which he had writen

--- BAL/Managers/CodeManager.cs
+++ BAL/Managers/CodeManager.cs
@@ -1,29 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using DAL.Interface;
 using Model.DB;
 using Model.DB.Code;
+using Model.DTO.CodeDTO;
 
 namespace BAL.Managers
 {
     public class CodeManager
     {
         private IUnitOfWork unitOfWork;
-        public CodeManager(IUnitOfWork unitOfWork)
+        private IMapper mapper;
+        public CodeManager(IUnitOfWork unitOfWork, IMapper mapper)
         {
+            this.mapper = mapper;
             this.unitOfWork = unitOfWork;
         }
 
-        public UserCode GetUserCodeById(string id)
+        public UserCodeDTO GetUserCodeById(string id)
         {
-            UserCode code = unitOfWork.CodeRepo.GetAll().FirstOrDefault(c => c.UserId == id);
+            UserCodeDTO code = mapper.Map<UserCodeDTO>(unitOfWork.CodeRepo.GetAll().FirstOrDefault(c => c.UserId == id));
             return code;
         }
 
-        public List<CodeResult> GetCodeResults(int codeId)
+        public List<CodeResultDTO> GetCodeResults(int codeId)
         {
-            List<CodeResult> results = unitOfWork.CodeResultsRepo.GetAll().ToList().Where(c => c.CodeId == codeId).ToList();
+            List<CodeResultDTO> results = mapper.Map<List<CodeResultDTO>>(unitOfWork.CodeResultsRepo.GetAll().ToList().Where(c => c.CodeId == codeId).ToList());
             return results;
+        }
+        public List<CodeErrorDTO> GetCodeErrors(int codeId)
+        {
+            List<CodeErrorDTO> results = mapper.Map<List<CodeErrorDTO>>(unitOfWork.CodeErrorsRepo.GetAll().ToList().Where(c => c.CodeId == codeId).ToList());
+            return results;
+        }
+
+        public UserCodeDTO UserCodeByExId(string userId, int exerciseId)
+        {
+            UserCodeDTO code = mapper.Map<UserCodeDTO>(unitOfWork.CodeRepo.GetAll().ToList().Find(c => c.UserId == userId && c.ExerciseId == exerciseId));
+            return code;
         }
     }
 }
--- WebApp/Controllers/CodeController.cs
+++ WebApp/Controllers/CodeController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BAL.Interfaces;
+using BAL.Managers;
+using DAL.Interface;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Model.DB;
 using Model.DB.Code;
@@ -11,16 +15,46 @@ namespace WebApp.Controllers
 {
     public class CodeController : Controller
     {
+        private IExerciseManager exerciseManager;
+        private IUnitOfWork unitOfWork;
+        public CodeController(IExerciseManager exerciseManager, IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            this.exerciseManager = exerciseManager;
+        }
         public IActionResult Index(UserCodeDTO model)
         {
+            if (!ModelState.IsValid)
+            {
+                return Redirect("Home/Index");
+            }
+
+            var exercise = exerciseManager.GetById(model.ExerciseId);
+            model.Exercise = exercise;
+            model.CodeText = exercise.TaskBaseCodeField;
+            var user = unitOfWork.UserRepo.Get(c => c.Email == model.UserId).First().Id;
+            model.UserId = user;
+            var findeCode = unitOfWork.CodeRepo.Get(c => c.UserId == user && c.ExerciseId == model.ExerciseId).Last();
+            if (findeCode != null)
+            {
+                model.CodeText = findeCode.CodeText;
+            }
+
             return View(model);
         }
 
         [HttpPost]
         public string GetCode(UserCodeDTO model)
         {
-            var text = model.CodeText;
-            return text;
+            UserCode code = new UserCode
+            {
+                CodeText = model.CodeText,
+                UserId = model.UserId,
+                ExerciseId = model.ExerciseId
+            };
+            unitOfWork.CodeRepo.Insert(code);
+            unitOfWork.Save();
+            return model.CodeText;
         }
     }
 }
