baaa3d37dfa600fd900d709a2884d8aaa993134f
BohdanKarpovych <ikarik1966@gmail.com>
2018-05-01 13:31:06 +0300

    #35 Hot fix

--- BAL/IoC/AutoMapperProfileConfiguration.cs
+++ BAL/IoC/AutoMapperProfileConfiguration.cs
@@ -71,8 +71,8 @@ namespace BAL.IoC
 
                 cfg.CreateMap<TestCase, TestCaseDTO>()
                    .ForMember(dest => dest.Id, options => options.MapFrom(src => src.Id))
-                   .ForMember(dest => dest.ExerciseDTOId, options => options.MapFrom(src => src.ExerciseId))
-                   .ForMember(dest => dest.UserDTOId, options => options.MapFrom(src => src.UserId))
+                   .ForMember(dest => dest.ExerciseId, options => options.MapFrom(src => src.ExerciseId))
+                   .ForMember(dest => dest.UserId, options => options.MapFrom(src => src.UserId))
                    .ForMember(dest => dest.InputData, options => options.MapFrom(src => src.InputData))
                    .ForMember(dest => dest.OutputData, options => options.MapFrom(src => src.OutputData))
                    .ForMember(dest => dest.Exercise, options => options.MapFrom(src => src.Exercise))
