7aa8c6379847531c22a25fb5d1ac1983b8bdbfd0
Bohdan Karpovych <ikarik1966@gmail.com>
2018-05-25 16:49:02 +0300

    Added BALTest project

---
+++ BALTest/NewsManagerTest.cs
@@ -0,0 +1,52 @@
+using AutoMapper;
+using BAL.IoC;
+using BAL.Managers;
+using DAL.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.DB;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BALTest
+{
+    [TestClass]
+    public class NewsManagerTest
+    {
+        IMapper mapper;
+        IUnitOfWork sUoW;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfileConfiguration());
+            });
+            mapper = config.CreateMapper();
+
+            var sNewsRepo = Substitute.For<IBaseRepository<News>>();
+            sUoW = Substitute.For<IUnitOfWork>();
+
+
+            var moqNews = new List<News>() { new News() { Text = "Hello World" } };
+            sNewsRepo.GetAll().Returns(moqNews);
+
+
+
+            sUoW.NewsRepo.Returns(sNewsRepo);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var newsManager = new NewsManager(sUoW, mapper);
+            var result = newsManager.GetAll();
+            sUoW.Received(1).Save();
+            sUoW.ClearReceivedCalls();
+
+            Assert.AreEqual("Hello World", result.First().Text);
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Model;
 using Model.DB;
 using BAL.IoC;
 using BAL.Interfaces;
