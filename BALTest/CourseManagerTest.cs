bbefb5704566ca25488012a5c9e9e206ee0de662
Bohdan Karpovych <ikarik1966@gmail.com>
2018-05-30 17:56:29 +0300

    Finished CourseManager tests

--- BALTest/CourseManagerTest.cs
+++ BALTest/CourseManagerTest.cs
@@ -2,19 +2,22 @@
 using BAL.IoC;
 using BAL.Managers;
 using DAL.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model.DB;
+using Model.DTO;
 using NSubstitute;
-using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BALTest
 {
-    [TestFixture]
-    class CourseManagerTest
+    [TestClass]
+    public class CourseManagerTest
     {
         IMapper mapper;
         IUnitOfWork sUoW;
 
-        [SetUp]
+        [TestInitialize]
         public void Setup()
         {
             var config = new MapperConfiguration(cfg =>
@@ -27,17 +30,76 @@ namespace BALTest
             sUoW = Substitute.For<IUnitOfWork>();
         }
 
-        static object[] TestCasesForGetAll =
+        [TestMethod]
+        public void InsertTest()
         {
-            new object[]{}
-        };
+            var courseManager = new CourseManager(sUoW, mapper);
+            courseManager.Insert(new CourseDTO() { Name = "aaa" });
+            courseManager.Insert(new CourseDTO() { Name = "bbb" });
+            sUoW.Received(2).Save();
+            sUoW.ClearReceivedCalls();
+        }
 
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var courseManager = new CourseManager(sUoW, mapper);
+            courseManager.Update(new CourseDTO() { Name = "aaa" });
+            sUoW.Received(1).Save();
+            sUoW.ClearReceivedCalls();
+        }
 
-        [Test, TestCaseSource("TestCasesForGetAll")]
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var courseManager = new CourseManager(sUoW, mapper);
+            courseManager.Delete(new CourseDTO() { Name = "aaa" });
+            sUoW.Received(1).Save();
+            sUoW.ClearReceivedCalls();
+        }
+
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            sUoW.CourseRepo.GetById(1).Returns(new Course() { Name = "aaa", UserId = "aaa" });
+            var courseManager = new CourseManager(sUoW, mapper);
+            Assert.AreEqual("aaa", courseManager.GetById(1).UserId);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            var courseManager = new CourseManager(sUoW, mapper);
+            sUoW.CourseRepo.Get(c => c.Name == "aaa").Returns(new List<Course>() { new Course() { Name = "aaa", UserId = "aaa" }, new Course() { Name = "aaa", UserId = "bbb" } });
+            var a = sUoW.CourseRepo.Get(c => c.Name == "aaa");
+            Assert.AreEqual("aaa", courseManager.Get(c => c.Name == "aaa").FirstOrDefault().Name);
+        }
+
+        [TestMethod]
         public void GetAllTest()
         {
             var courseManager = new CourseManager(sUoW, mapper);
-            var result = courseManager.GetAll();
+            sUoW.CourseRepo.GetAll().Returns(new List<Course>() { new Course() { Name = "aaa" }, new Course() { Name = "bbb" } });
+            Assert.AreEqual("aaa", courseManager.GetAll().FirstOrDefault().Name);
+        }
+
+        [TestMethod]
+        public void UpdateCourseOwnerTest()
+        {
+            sUoW.CourseRepo.GetById(1).Returns(new Course() { Name = "aaa", UserId = "aaa" });
+            var courseManager = new CourseManager(sUoW, mapper);
+            courseManager.UpdateCourseOwner(1, "bbb");
+            Assert.AreEqual("bbb", courseManager.GetById(1).UserId);
+        }
+
+
+        [TestMethod]
+        public void ToggleCourseStatusTest()
+        {
+            var courseManager = new CourseManager(sUoW, mapper);
+            sUoW.CourseRepo.GetById(1).Returns(new Course() { Name = "aaa", UserId = "aaa", IsActive = true });
+            courseManager.ToggleCourseStatus(1);
+            Assert.AreEqual(false, courseManager.GetById(1).IsActive);
         }
     }
 }
